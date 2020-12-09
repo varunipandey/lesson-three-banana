56f408220037ca028221c752f30e6a0a76a95769
Varuni Pandey <14542649+varunipandey@users.noreply.github.com>
2020-12-09 18:49:24 +0530

    update: translate button

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // alert("Hello How are you?");
 console.log("Hello from the app.js page");
 
+function clickHandler() {
+    console.log("Translate button Clicked");
+};
 
+var btnTranslate = document.querySelector("#btn-translate");
+// console.log(btnTranslate)
+
+btnTranslate.addEventListener("click", clickHandler);
 
 
