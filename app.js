7f597105f0591e2618ead1696f9fb26b8bcec59c
Varuni Pandey <14542649+varunipandey@users.noreply.github.com>
2020-12-09 19:33:02 +0530

    input output baiscs done

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ console.log("Hello from the app.js page");
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output");
 // console.log(txtInput)
 
+// outputDiv.innerText = "Varuni Pandey"
+
 function clickHandler() {
-    console.log("Translate button Clicked");
-    console.log("Input: "+txtInput.value)
+    // console.log("Translate button Clicked");
+    // console.log("Input: "+txtInput.value)
+    outputDiv.innerText = "hgbfjrgfksbrffgikin " + txtInput.value;
 };
 
 
