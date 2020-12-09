17dd327d980a3eb0e38797a7efd5894e56d95f6b
Varuni Pandey <14542649+varunipandey@users.noreply.github.com>
2020-12-09 21:46:29 +0530

    translated api added

--- app.js
+++ app.js
@@ -8,15 +8,34 @@ var outputDiv = document.querySelector("#output");
 
 // outputDiv.innerText = "Varuni Pandey"
 
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverUrl = "https://api.funtranslations.com/translate/"
+
+function getTranslationUrl(text) {
+    return serverUrl + "?text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured ", error)
+    alert("Something wrong with server! try again after some time.")
+}
+
 function clickHandler() {
     // console.log("Translate button Clicked");
     // console.log("Input: "+txtInput.value)
-    outputDiv.innerText = "hgbfjrgfksbrffgikin " + txtInput.value;
+    // outputDiv.innerText = "hgbfjrgfksbrffgikin " + txtInput.value;
+    var translationInput = txtInput.value;
+    fetch(getTranslationUrl(translationInput))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translated;
+        })
+        .catch(errorHandler)
 };
 
 
 // console.log(btnTranslate)
 
 btnTranslate.addEventListener("click", clickHandler);
-
-
