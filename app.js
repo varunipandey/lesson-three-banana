0fbfd127b18f5f9aaf1d23a337b468bb1d5b1aa6
Varuni Pandey <14542649+varunipandey@users.noreply.github.com>
2020-12-09 19:05:21 +0530

    taking input

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 // alert("Hello How are you?");
 console.log("Hello from the app.js page");
 
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input")
+// console.log(txtInput)
+
 function clickHandler() {
     console.log("Translate button Clicked");
+    console.log("Input: "+txtInput.value)
 };
 
-var btnTranslate = document.querySelector("#btn-translate");
+
 // console.log(btnTranslate)
 
 btnTranslate.addEventListener("click", clickHandler);
